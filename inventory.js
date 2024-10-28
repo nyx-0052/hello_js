0431f8ff5693cb4b2314a6b20f684d6a69cbc46f
nyx-0052 <elsali0752@gmail.com>
2024-10-28 15:32:25 +0000

    feat: completed extension task 1.

--- inventory.js
+++ inventory.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Hello World!");
 // Enum for category of items
 var Category;
@@ -7,14 +8,14 @@ var Category;
     Category["Clothing"] = "Clothing";
 })(Category || (Category = {}));
 // Example items
-var broccoli = {
+const broccoli = {
     id: 1,
     name: "broccoli",
     category: Category.Grocery,
     price: 60,
     stock: 100,
 };
-var laptop = {
+const laptop = {
     id: 2,
     name: "laptop",
     category: Category.Electronics,
@@ -39,8 +40,13 @@ function printItemDetails(item) {
     else {
         console.log("Warranty Period:", item.warrantyPeriodMonths, "months");
     }
-    console.log("Warranty Period: ".concat((_a = item.warrantyPeriodMonths) !== null && _a !== void 0 ? _a : "NA", " months"));
+    console.log(`Warranty Period: ${(_a = item.warrantyPeriodMonths) !== null && _a !== void 0 ? _a : "NA"} months`);
 }
 printItemDetails(broccoli);
 console.log("-------");
 printItemDetails(laptop);
+// Extensions
+// 1. Function to add a new item to the inventory
+// 2. Function to update the stock of an item
+// 3. Function to find items by category (input: 'Electronics', output: all electronic items)
+// 4. Function to calculate the total price of items in stock (input: 'item', price x stock, output: total price of items in stock)
--- inventory.ts
+++ inventory.ts
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+import promptSync from 'prompt-sync';
 
 // Enum for category of items
 enum Category {
@@ -39,27 +39,53 @@ const laptop: Item = {
 // input: item: Item
 // output: return void
 // console
-function printItemDetails(item: Item){
+function printItemDetails(item: Item) {
     console.log("ID:", item.id);
     console.log("Name:", item.name);
     console.log(item.category);
     console.log(item.price);
     console.log(item.stock);
-    if(item.warrantyPeriodMonths === undefined){
+    if (item.warrantyPeriodMonths === undefined) {
         console.log("Warranty Period: NA months");
-    } else{
+    } else {
         console.log("Warranty Period:", item.warrantyPeriodMonths, "months");
     }
 
-    console.log(`Warranty Period: ${item.warrantyPeriodMonths ?? "NA"} months`);
+    //console.log(`Warranty Period: ${item.warrantyPeriodMonths ?? "NA"} months`);
+}
+
+function addNewItem(): Item {
+    const prompt = promptSync();
+    const id = Number(prompt('Enter the ID '));
+    const name = prompt('Enter the name ');
+    const category = prompt('Enter the category ');
+    const price = Number(prompt('Enter the price '));
+    const stock = Number(prompt('Enter the stock '));
+    const warrantyPeriodMonths = Number(prompt('Enter the number of months '));
+
+    const item: Item = {
+        id: id,
+        name: name,
+        category: category as Category,
+        price: price,
+        stock: stock,
+        warrantyPeriodMonths: warrantyPeriodMonths,
+    }
+
+    printItemDetails(item);
+    return item;
 }
 
 printItemDetails(broccoli);
 console.log("-------")
 printItemDetails(laptop);
+console.log("-------");
+addNewItem();
+
+
 
 // Extensions
-// 1. Function to add a new item to the inventory
+// ✅ 1. Function to add a new item to the inventory (input: the list of attributes of the item, output: item)
 // 2. Function to update the stock of an item
 // 3. Function to find items by category (input: 'Electronics', output: all electronic items)
 // 4. Function to calculate the total price of items in stock (input: 'item', price x stock, output: total price of items in stock)
